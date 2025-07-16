56ba424561b83481bd0b0cc8826f1fbba7f36169
walshak123 <patrickwalshak1@gmail.com>
2025-07-16 18:57:39 +0100

    Just created a simple calculator

---
+++ my-calculator.py
@@ -0,0 +1,59 @@
+print('''🫠🤑😈🫠🤑😈🫠🤑😈🫠🤑😈🫠
+#1. Addition
+#2. Subtraction
+#3. Multiplication
+#4. Division
+#5. Floor division
+#🫠🤑😈🫠🤑😈🫠🤑😈🫠🤑😈🫠''')
+print("")
+print("ADDITION")
+print("Enter two number to add")
+first_number = input("first_number:")
+second_number = input("second_number:")
+
+sum = float(first_number) / float(second_number)
+print(f"{first_number} / {second_number} = {sum:.2f}")
+
+print("")
+print("SUBTRACTION")
+print("🫠********************🫠")
+print("Enter two number to subtract")
+first_number = input("first_number:")
+second_number = input("second_number:")
+
+sum = float(first_number) // float(second_number)
+print(f"{first_number} //  {second_number} = {sum:.2f}")
+
+print("")
+print("MULTIPLCATION")
+print("🫠*********************🫠")
+print("Enter two number to multiply")
+first_number = input("first_number:")
+second_number = input("second_number:")
+ 
+mul = float(first_number) / float(second_number)
+print(f"{first_number} / {second_number} = {mul:.2f}")
+
+
+print("")
+print("DIVISION")
+print("🫠********************🫠")
+print("Enter two number to divid")
+first_number = input("first_number:")
+second_number = input("second_number:")
+ 
+div = float(first_number) / float(second_number)
+print(f"{first_number} / {second_number} = {div:.2f}")
+
+
+print("")
+print("EXPONENTIAL")
+print("🫠********************🫠")
+print("Enter two number")
+first_number = input("first_number:")
+second_number = input("second_number:")
+ 
+exp = float(first_number) // float(second_number)
+print(f"{first_number} // {second_number} = {exp:.2f}")
+
+
